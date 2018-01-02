d277f8cb15bcfa89147ef07071d7a33b7fccd27c
CpuKnows <jamaxwell1990@gmail.com>
2018-01-01 21:12:26 -0600

    Attempt to fix procfile

--- src/server.py
+++ src/server.py
@@ -31,5 +31,5 @@ if __name__ == '__main__':
     warnings.filterwarnings('ignore', '.*returning an arbitrary sample.*',)
 
     bot = chatbot.Bot()
-
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
